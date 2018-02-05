eb91254277c68de516a3072eb1e8624f050f725c
xxh <xxh332211@qq.com>
2018-02-05 08:32:13 +0800

    1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { ShowcaseComponent } from './view/showcase/showcase.component';
 import { SayholloComponent } from './view/sayhollo/sayhollo.component';
 import { TodomvcComponent } from './view/todomvc/todomvc.component';
 import { PagenotfoundComponent } from './view/pagenotfound/pagenotfound.component';
+import { TodoDetailComponent } from './view/todomvc/todo-detail/todo-detail.component';
 const routes: Routes = [
     {
         path: '',
@@ -13,7 +14,8 @@ const routes: Routes = [
     { path: 'showcase', component: ShowcaseComponent },
     { path: 'sayhollo', component: SayholloComponent },
     { path: 'todomvc', component: TodomvcComponent },
-    { path: '**', component: PagenotfoundComponent }
+    // { path: '**', component: PagenotfoundComponent },
+    { path: 'todo/:id', component: TodoDetailComponent}
 ];
 
 @NgModule({
